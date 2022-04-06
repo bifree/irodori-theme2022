68b22724eed1ee991eb91f1717bd2e1bb9b0ad6f
bifree <bifree2bfree@gmail.com>
2022-04-06 11:56:09 +0900

    serviceテンプレート追加

---
+++ page-srv.php
@@ -0,0 +1,9 @@
+<?php
+/*Template Name: Service*/
+?>
+
+<?php get_header(); ?>
+<?php get_template_part('overlay'); ?>
+<?php get_template_part('page_top'); ?>
+<?php the_content() ?>
+<?php get_footer(); ?>
