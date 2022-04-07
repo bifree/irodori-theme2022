518a264bdb51ad31fbe4ab073e9276289b321db8
bifree <bifree2bfree@gmail.com>
2022-04-07 10:24:57 +0900

    service

--- page-srv.php
+++ page-srv.php
@@ -4,6 +4,11 @@
 
 <?php get_header(); ?>
 <?php get_template_part('overlay'); ?>
-<?php get_template_part('page_top'); ?>
+<div class="title_bg">
+	<img src="http://irodori-odori.com/wp-content/uploads/2021/08/title_background.png" alt="">
+	<h2 class="page__title">
+		SERVICE
+	</h2>
+</div>
 <?php the_content() ?>
 <?php get_footer(); ?>
